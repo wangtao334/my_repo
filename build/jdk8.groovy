47f82bb46243347600cf7d03671924a13daa93b1
wangtao334 <wangtao-334@163.com>
2017-12-23 22:14:42 +0800

    Update jdk8.groovy

--- build/jdk8.groovy
+++ build/jdk8.groovy
@@ -1,8 +1,5 @@
 node {
-  def mvnHome
   stage('Preparation') {
-    println "${MAVEN_HOME}"
-    mvnHome = tool 'M3'
-    println "mvnHome = " + mvnHome
+    println "MAVEN_HOME = " + MAVEN_HOME
   }
 }
