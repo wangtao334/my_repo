14227a7617b4fab71cff178e919f3deafb66cdde
wangtao334 <wangtao-334@163.com>
2017-12-24 13:40:20 +0800

    Update jdk8.groovy

--- build/jdk8.groovy
+++ build/jdk8.groovy
@@ -21,7 +21,7 @@ node {
         for(int k = 0; k < files.size(); k++) {
           def file = files[k]
           def projectName = file.path.substring(0, file.path.indexOf("/"))
-          if(projectName.startsWith("build")) {
+          if(projectName.startsWith("build00")) {
             continue
           }
           def filePath = file.path.substring(file.path.indexOf("/"))
@@ -50,7 +50,7 @@ node {
     stage('Build') {
       changeMap.each {
         key,value -> 
-          echo '${ANT_HOME}/bin/ant -f ${WORKSPACE}/${key}/build.xml'
+          echo '${ANT_HOME}'
       }
     }
   }
