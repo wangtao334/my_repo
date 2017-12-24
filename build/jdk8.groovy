0d35fcdb78d46e57f7dbaa7db48c50ba86128201
wangt <wangt@DESKTOP-OTEH41V.mshome.net>
2017-12-24 09:31:50 +0800

    000

--- build/jdk8.groovy
+++ build/jdk8.groovy
@@ -3,20 +3,6 @@ node {
     println "MAVEN_HOME = " + MAVEN_HOME
     println "WORKSPACE = " + WORKSPACE
     sh 'mvn -version'
-    
-    // check change.
-    def changeLogSets = currentBuild.changeSets
-    for(int i = 0; i < changeLogSets.size(); i++) {
-      def entries = changeLogSets[i].items
-      for(int j =0; j < entries.length; j++) {
-        def entry = entries[j]
-        def files = new ArrayList(entry.affectedFiles)
-        for(int k = 0; k < files.size(); k++) {
-          def file = files[k]
-          println file.editType.name + " : " + file.path
-        }
-      }
-    }
   }
   
   stage('Checkout') {
