9f2a9b9c54096671097d7fd1b5c5319b87e1f39d
wangt <wangt@DESKTOP-OTEH41V.mshome.net>
2017-12-24 09:25:28 +0800

    dfdf

--- build/jdk8.groovy
+++ build/jdk8.groovy
@@ -5,7 +5,7 @@ node {
     sh 'mvn -version'
     
     // check change.
-    def changeLogSets = currentBuild.rawBuild.changeSets
+    def changeLogSets = currentBuild.changeSets
     for(int i = 0; i < changeLogSets.size(); i++) {
       def entries = changeLogSets[i].items
       for(int j =0; j < entries.length; j++) {
