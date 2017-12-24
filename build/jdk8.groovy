dc574bc300c0c4ff149372673cabe7cef82dc8c3
wangtao334 <wangtao-334@163.com>
2017-12-24 12:39:33 +0800

    Update jdk8.groovy

--- build/jdk8.groovy
+++ build/jdk8.groovy
@@ -1,4 +1,4 @@
- node {
+{ node {
   stage('Preparation') {
     println "MAVEN_HOME = " + MAVEN_HOME
     println "WORKSPACE = " + WORKSPACE
@@ -32,8 +32,12 @@
         }
       }
     }
-    changeMap.each{
-      entry -> println entry.key
+    changeMap.each {
+      projectNameEntry -> {
+        fileEntry.value.each {
+          println fileEntry.key + " : " + fileEntry.value
+        }
+      }
     }
   }
 }
